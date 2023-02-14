f34b6cc3c29a8b2a851182a8f3e7992b9b9316ff
Mazene <mmazenezerguine@gmail.com>
2023-02-14 22:43:04 +0100

    feat : selecting one book by name and by id

--- backend/src/Controller/BooksController.php
+++ backend/src/Controller/BooksController.php
@@ -15,8 +15,8 @@ class BooksController extends AbstractController
     #[Route('/api/books', name: 'app_books_get_all' , methods:['GET'])]
     public function getAll(ManagerRegistry $doctrine): JsonResponse
     {   
-        $books = $doctrine->getRepository(Books::class)->findAll() ; 
-        $result = array(); 
+        $books = $doctrine->getRepository(Books::class)->findAll();
+        $result = array();
         foreach($books as $key => $value){
             $result[$key]  = [
                "title" => $value->getTitle(),
@@ -33,4 +33,53 @@ class BooksController extends AbstractController
             "data" => $result 
         ]);
     }
+
+    #[Route('/api/books/{id}', name: 'app_books_get_one' , methods:['GET'])]
+    public function getOneBook(ManagerRegistry $doctrine, $id): JsonResponse{
+
+        if (gettype($id) === "integer") {
+            $book = $doctrine->getRepository(Books::class)->find($id);
+
+            if (!$book) {
+                return $this->json([
+                    "response_code" => 400 , 
+                    "message" => "book not found (try with an other id)"
+                ]);
+            }
+            $result = [
+                "title" => $book->getTitle(),
+                "author" => $book->getAuthor(),
+                "overview" =>  $book->getOverView() ,
+                "picture" => $book->getpicture(),
+                "created_at" => $book->getCreatedAt(),
+                "updated_at" => $book->getupdatedAt()
+            ] ; 
+            return $this->json([
+                "response_code" => 200 ,
+                "message" => "" , 
+                "data" => $result 
+            ]);
+        } else {
+            $book = $doctrine->getRepository(Books::class)->findByName($id);
+
+            if (!$book) {
+                return $this->json([
+                    "response_code" => 400 , 
+                    "message" => "book not found (try with an other id)"
+                ]);
+            }
+            $result = [
+                "title" => $book[0]->getTitle(),
+                "author" => $book[0]->getAuthor(),
+                "overview" =>  $book[0]->getOverView() ,
+                "picture" => $book[0]->getpicture(),
+                "created_at" => $book[0]->getCreatedAt(),
+                "updated_at" => $book[0]->getupdatedAt()
+            ] ; 
+
+            return $this->json($result) ;
+        } 
+
+    }
+
 }
--- backend/src/Repository/BooksRepository.php
+++ backend/src/Repository/BooksRepository.php
@@ -39,20 +39,19 @@ class BooksRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Books[] Returns an array of Books objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('b.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+    * @return Books[] Returns an array of Books objects
+    */
+   public function findByName($name): array
+   {
+       return $this->createQueryBuilder("books")
+           ->andWhere('books.title = :val')
+           ->setParameter('val', $name)
+           //->orderBy('b.id', 'ASC')
+           //->setMaxResults(10)
+           ->getQuery()
+           ->getResult();
+   }
 
 //    public function findOneBySomeField($value): ?Books
 //    {
