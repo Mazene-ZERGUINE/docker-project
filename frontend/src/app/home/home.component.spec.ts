d11901d991f38e5acbc91155e31152048c77f66c
LoicZHU <zhu.loic@gmail.com>
2023-02-22 21:03:58 +0100

    test: fix home spec

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { ComponentsModule } from '../shared/components/components.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,6 +11,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      imports: [ComponentsModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
