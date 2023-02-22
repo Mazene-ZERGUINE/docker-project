adcc67132cea4c532c809321d90b45e292d8561c
LoicZHU <zhu.loic@gmail.com>
2023-02-22 21:05:10 +0100

    test: fix app spec

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ToastModule } from './shared/components/toast/toast.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, ToastModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
