6e5c3474e7a58537c80214570f6af320a6f9a9eb
LoicZHU <zhu.loic@gmail.com>
2023-02-22 20:50:14 +0100

    test: fix book-details spec

--- frontend/src/app/books/book-details/book-details.component.spec.ts
+++ frontend/src/app/books/book-details/book-details.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookDetailsComponent } from './book-details.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BookDetailsComponent', () => {
   let component: BookDetailsComponent;
@@ -9,6 +11,7 @@ describe('BookDetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BookDetailsComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BookDetailsComponent);
